756573598cd1fc86967c4c0b4f27cde0553e196b
Gauree <gaure45@gmail.com>
2015-04-11 22:03:51 -0500

    incremental updates

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,11 +2,11 @@ import urllib.request
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
-
+base_url='https://www.eventbriteapi.com/v3/events/search/?token=BKKRDKVUVRC5WG4HAVLT&'
 
 def index(request):
 	url='https://www.eventbriteapi.com/v3/categories/?token=BKKRDKVUVRC5WG4HAVLT'
@@ -33,7 +33,7 @@ def event1(request):
 	list=request.get_full_path() 
 	split =list.split('?')
 	print (split[1])
-	base_url='https://www.eventbriteapi.com/v3/events/search/?'+split[1]+'&token=BKKRDKVUVRC5WG4HAVLT'
+	base_url='https://www.eventbriteapi.com/v3/events/search/?'+split[1]+'&token=BKKRDKVUVRC5WG4HAVLT&format=json'
 	response=urllib.request.urlopen(base_url)
 	str_response=response.readall().decode('utf-8')
 	data=json.loads(str_response)
@@ -41,6 +41,7 @@ def event1(request):
 	pagination_obj=data['pagination']
 	page_count=pagination_obj['page_count']
 	page_size=pagination_obj['page_size']
+	'''
 	for i in range(0,page_count):
 		print (i)
 		page_url='https://www.eventbriteapi.com/v3/events/search/?'+split[1]+'&token=BKKRDKVUVRC5WG4HAVLT'+'&page='+str(i)
@@ -48,27 +49,74 @@ def event1(request):
 		data=json.loads(str_response)
 		event_list.append(data['events'])
 		print(page_url)
+	'''
 	return event_list
 	
 	
+
+def getevents(url,set_max_pages):
 	
-def page_count(request)
+	
+	response=urllib.request.urlopen(base_url+url)
+	str_response=response.readall().decode('utf-8')
+	data=json.loads(str_response)
+	event_list=data['events']
+	return event_list
 
 
-return
-
-
-
-
-
+	
+def get_max_pages(url):
+	response=urllib.request.urlopen(base_url+url)
+	str_response=response.readall().decode('utf-8')
+	data=json.loads(str_response)
+	pagination_obj=data['pagination']
+	max_pages1=pagination_obj['page_count']
+	print (max_pages1)
+	return max_pages1
 	
 def events(request):
-	list=request.get_full_path() 
-	if 'categories' in list:
-		list=event1(request)
+	
+	set_max_page_count='false'
+	current_page_uri=request.get_full_path()
+	query_string=current_page_uri.split('?')
+	get_page_no = request.GET.get('page')
+	valid_query='true'
+	max_pages=get_max_pages(query_string[1])
+	try:
+		current_page_no = int(get_page_no)
+	except TypeError:
+		current_page_no = 1
+		set_max_page_count='true'
+	
+	if current_page_no is 1:
+		next_page=current_page_uri+'&page=2'
+		previous_page=0
+	elif current_page_no is max_pages:
+		print (current_page_no)
+		print (max_pages)
+		next_page=0
+		pageless_uri=current_page_uri.split('page')
+		previous_page_no=int(current_page_no)-1
+		previous_page=pageless_uri[0]+'page='+str(previous_page_no)
+	elif current_page_no > max_pages:
+		previous_page=0
+		next_page=0
+		valid_query='false'
+	else:
+		next_page_no=int(current_page_no)+1
+		previous_page_no=int(current_page_no)-1
+		pageless_uri=current_page_uri.split('page')
+		next_page=pageless_uri[0]+'page='+str(next_page_no)
+		previous_page=pageless_uri[0]+'page='+str(previous_page_no)
+	
+	if  valid_query is 'true':
+		events=getevents(query_string[1],set_max_page_count)
+	else:
+		raise Http404("Page Not Found")
+	
 			
 
-	return render(request,'helloworld/events.html', { 'hello' :list })
+	return render(request,'helloworld/events.html', { 'events' :events,'previous_page':previous_page,'next_page' : next_page,'current_page_no':current_page_no,'max_pages':max_pages})
 	
 
 	
