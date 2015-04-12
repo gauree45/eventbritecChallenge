0bda38f4bde6f6807c649ac862f18f3e1a0d608a
Gauree <gaure45@gmail.com>
2015-04-11 23:30:56 -0500

    Last working changes

---
+++ README.txt
@@ -0,0 +1,8 @@
+Features supported:
+1.Listing of events using the categories from the EventBrite API
+2.List all the relevent events for the selected categories with pagination
+
+Features To Do:
+1. Javascript to limit the number of categories to 3.
+2. Render a custom bad request page if the category entered in the URL does not exist
+3. Generate Authentication tokein for the specific user
--- helloworld/views.py
+++ helloworld/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render
 from django.http import Http404
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+#This is the base url for the event search , query_string is appended to it
 base_url='https://www.eventbriteapi.com/v3/events/search/?token=BKKRDKVUVRC5WG4HAVLT&'
 
+#This function grabs the categories from the api and displays on index.html
 def index(request):
+	#This url provides the categories to the user for selection
 	url='https://www.eventbriteapi.com/v3/categories/?token=BKKRDKVUVRC5WG4HAVLT'
 	response=urllib.request.urlopen(url)
 	str_response=response.readall().decode('utf-8')
@@ -30,9 +32,8 @@ def index(request):
 
 
 
-
+#This function provides the api page results for events function 
 def getevents(url,set_max_pages):
-	
 	
 	response=urllib.request.urlopen(base_url+url)
 	str_response=response.readall().decode('utf-8')
@@ -41,7 +42,7 @@ def getevents(url,set_max_pages):
 	return event_list
 
 
-	
+#This function calculates the maximum pages
 def get_max_pages(url):
 	response=urllib.request.urlopen(base_url+url)
 	str_response=response.readall().decode('utf-8')
@@ -51,6 +52,7 @@ def get_max_pages(url):
 	print (max_pages1)
 	return max_pages1
 	
+#This function grabs the categories from the api and displays on events.html
 def events(request):
 	
 	set_max_page_count='false'
@@ -58,16 +60,18 @@ def events(request):
 	query_string=current_page_uri.split('?')
 	get_page_no = request.GET.get('page')
 	valid_query='true'
+	#calculates the maximum number of pages for the query_string
 	max_pages=get_max_pages(query_string[1])
 	try:
 		current_page_no = int(get_page_no)
 	except TypeError:
 		current_page_no = 1
 		set_max_page_count='true'
-	
+	#checks if the current page is 1 then disables previous button
 	if current_page_no is 1:
 		next_page=current_page_uri+'&page=2'
 		previous_page=0
+	#checks if the current page is the last page then disables next button
 	elif current_page_no is max_pages:
 		print (current_page_no)
 		print (max_pages)
@@ -75,10 +79,12 @@ def events(request):
 		pageless_uri=current_page_uri.split('page')
 		previous_page_no=int(current_page_no)-1
 		previous_page=pageless_uri[0]+'page='+str(previous_page_no)
+	#checks for page number greater than the maximum page number then throws page not found
 	elif current_page_no > max_pages:
 		previous_page=0
 		next_page=0
 		valid_query='false'
+	#checks for pages in the valid range
 	else:
 		next_page_no=int(current_page_no)+1
 		previous_page_no=int(current_page_no)-1
@@ -86,13 +92,14 @@ def events(request):
 		next_page=pageless_uri[0]+'page='+str(next_page_no)
 		previous_page=pageless_uri[0]+'page='+str(previous_page_no)
 	
+	#valid query is the one where pageno<maximum page number for the query_string 
 	if  valid_query is 'true':
 		events=getevents(query_string[1],set_max_page_count)
 	else:
 		raise Http404("Page Not Found")
 	
 			
-
+	#The events url for the previous page,url for the next page ,current page numer and maximum page number are sent to the template
 	return render(request,'helloworld/events.html', { 'events' :events,'previous_page':previous_page,'next_page' : next_page,'current_page_no':current_page_no,'max_pages':max_pages})
 	
 
