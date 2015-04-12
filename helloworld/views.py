becae84d061393041bfbd307bf2018530e455dbd
Gauree <gaure45@gmail.com>
2015-04-11 23:07:09 -0500

    working good

--- helloworld/views.py
+++ helloworld/views.py
@@ -26,33 +26,10 @@ def index(request):
 		categorylist.update({id:catname})
 	
 	print (categorylist)
-	return render(request, 'helloworld/index.html',{'cat': categorylist})
+	return render(request, 'helloworld/index.html',{'cat':categorylist})
 
 
-def event1(request):
-	list=request.get_full_path() 
-	split =list.split('?')
-	print (split[1])
-	base_url='https://www.eventbriteapi.com/v3/events/search/?'+split[1]+'&token=BKKRDKVUVRC5WG4HAVLT&format=json'
-	response=urllib.request.urlopen(base_url)
-	str_response=response.readall().decode('utf-8')
-	data=json.loads(str_response)
-	event_list=data['events']
-	pagination_obj=data['pagination']
-	page_count=pagination_obj['page_count']
-	page_size=pagination_obj['page_size']
-	'''
-	for i in range(0,page_count):
-		print (i)
-		page_url='https://www.eventbriteapi.com/v3/events/search/?'+split[1]+'&token=BKKRDKVUVRC5WG4HAVLT'+'&page='+str(i)
-		response=urllib.request.urlopen(page_url)
-		data=json.loads(str_response)
-		event_list.append(data['events'])
-		print(page_url)
-	'''
-	return event_list
-	
-	
+
 
 def getevents(url,set_max_pages):
 	
